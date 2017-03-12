548d55032c1f9e9e8eb433ace12e4d8e950708dc
ValHall <Valynhall@gmail.com>
2017-03-12 15:54:19 -0400

    created time function to calculate next occurance

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,22 +11,46 @@
 
   var database = firebase.database();
 
+  // function to calculate the next occurance of train
+  function getNextOccurance(startTime, frequency) {
+    // create a moment time based on military time input
+    var startdateTime = moment(startTime, "HH:mm");
 
+    // while this next occurance is in the past
+    while (moment().diff(startdateTime) > 0) {
+      // add the next frequency
+      startdateTime = startdateTime.add(frequency, 'minutes');
+    }
+
+    // calculate the difference between now and the next occurance
+    var duration = moment.duration(moment().diff(startdateTime)*(-1));
+    // report the time in minutes
+    var minutesUntilArrival = parseInt(duration.asMinutes()+1);
+    return minutesUntilArrival;
+  }
+
+  // refresh the table
   database.ref("/trains").on("value", function(snapshot) {
       $("#mainTableBody").empty();
       snapshot.forEach(function(data) {
+        // calculate the minutes until next arrival
+        var minsUntilArrival = getNextOccurance(data.val().firstTrainTime, data.val().frequency)
+
+        // append the row to the table
         $("#mainTableBody").append(
             "<tr>" +
             "<td>"+ data.val().trainName + "</td>" +
             "<td>"+ data.val().destination + "</td>" +
             "<td>"+ data.val().frequency + "</td>" +
-            "<td>"+ "-" + "</td>" +
-            "<td>"+ "-" + "</td>" +
+            // format the next arrival time in local time
+            "<td>"+ moment().add(minsUntilArrival, "minutes").format("h:mm a") + "</td>" +
+            // display the calculated minutes until next arrival
+            "<td>"+ minsUntilArrival + "</td>" +
              + "</tr>"
         );
       });
   }, function(errorObject) {
-  console.log("Errors handled: " + errorObject.code);
+    console.log("Errors handled: " + errorObject.code);
   });
 
   $("#addTrain").on("click", function() {
@@ -39,8 +63,8 @@
 
     trainName = $("#name-input").val().trim();
     destination = $("#destination-input").val().trim();
-    frequency = $("#time-input").val().trim();
-    firstTrainTime= $("#frequency-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
+    firstTrainTime= $("#time-input").val().trim();
 
     database.ref("/trains").push({
         trainName: trainName,
